85a0dd59879bcf27a645d2d8f19599f3f20a8d82
Ashutosh Misra <ashutosh.mishra@amd.com>
2021-07-26 12:44:19 +0530

    Modified User intervention for processing options   Clean up

--- AddExecutionTimeToProf.sh
+++ AddExecutionTimeToProf.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+
+#constants for using
+CWF="$PWD/workingdir"
+ETF="ExecutionTime"
+NFET="$1_with_$ETF.csv"
+EXTM="NONE"  #Execution Time Method
+CPPEXE="FileProcessor"
+
+
+echo
+echo
 echo "Profiler Output File given = $1 "
 
 if [ ! -e "$1" ] || [ ! -s "$1" ] ; then
@@ -7,12 +18,10 @@ if [ ! -e "$1" ] || [ ! -s "$1" ] ; then
     exit 1
 fi
 
-#constants for using
-CWF="$PWD/workingdir"
-ETF="ExecutionTime"
-NFET="$1_with_$ETF.csv"
-EXTM="NONE"  #Execution Time Method
-
+if [ ! -e "$1" ] || [ ! -s "$1" ] ; then
+    echo "Fatal !!!   $CPPEXE is empty or does not exists "
+    exit 1
+fi
 
 echo
 echo "Creating working dir "
@@ -23,53 +32,70 @@ if [ $? -ne 0 ] ;then
     exit 1
 fi
 
+declare -a ExecutionOptions=("Execution Time = Endtime - StartTime" "Execution Time = CompletionTime -  SubmissionTime")
+declare -a ExecutionTimeEXTM=("End_Start" "Complete_Dispatch")
+
 echo
 echo "Calculating Execution Time "
-echo
-echo "Options for calculating the execution time "
-echo
-echo "1. Execution Time = Endtime - StartTime "
-echo "2. Execution Time = CompletionTime -  SubmissionTime "
-echo
-echo "Choice ( 1/2 ) "
-
-read CHOICE
-
-echo
-echo "Processing ..... "
-echo
-
-if [ $CHOICE -eq 1 ]; then
-     EXTM="End_Start"
-     awk -F, '{ print $(NF-1) - $(NF-2) }' $1 > $CWF/$ETF
-elif [ $CHOICE -eq 2 ]; then
-     EXTM="Complete_Dispatch"
-     awk -F, '{ print $NF - $(NF-3) }' $1 > $CWF/$ETF
-else
-   echo "Wrong choice "
-   exit 1
-fi
-
-NFET="$1_with_$ETF$EXTM.csv"
-
-echo "Done .. "
-
-sed -i "1s/.*/$ETF/" $CWF/$ETF
 
 
-echo
-echo "Adding Execution Time to Profiler Data ..."
-paste -d',' $CWF/$ETF $1 > "$NFET"
-echo "Done... "
+LoopCounter=0
+OutputFiles=()
+
+for i in "${ExecutionOptions[@]}"
+do
+    echo
+    echo "$i"
+    EXTM="${ExecutionTimeEXTM[$LoopCounter]}"
+    echo
+    echo "Processing ..... "
+    echo
+
+    if [ $LoopCounter -eq 0 ]; then
+        awk -F, '{ print $(NF-1) - $(NF-2) }' $1 > $CWF/$ETF
+    elif [ $LoopCounter -eq 1 ]; then
+         awk -F, '{ print $NF - $(NF-3) }' $1 > $CWF/$ETF
+    fi
+
+    ((LoopCounter=LoopCounter+1))
+    echo
+    echo "Done .. "
+
+    #change the header of the ExecutionTime
+    sed -i "1s/.*/$ETF/" $CWF/$ETF
+
+    NFET="$1_with_$ETF$EXTM.csv"
+
+    echo
+    echo "Adding Execution Time to Profiler Data ..."
+    paste -d',' $CWF/$ETF $1 > $NFET
+    echo
+    echo "Done... "
+    echo
+    echo "Output File is $NFET "
+
+    OutputFiles+=("$NFET")
+done
+
+./$CPPEXE 4 "${OutputFiles[0]}" "${OutputFiles[1]}"
+
 
 
 echo
 echo "Removing temps "
 rm -rf $CWF
+rm ${OutputFiles[0]}
+rm ${OutputFiles[1]}
 
-echo "Sucessfully Completed"
 
-echo "Output File is $NFET "
+echo
+echo
+
+if [ $? -eq 0 ]; then
+    echo "Sucessfully Completed"
+else
+    echo "There was an error in processing "
+fi
 
 
 
